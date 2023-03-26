e87e6716d004b8dfe1e7155d1c7751e41d1af7ab
Vitaly Tregubov <vit.tregubov@gmail.com>
2023-03-26 12:12:06 +0200

    feat(localization): add bundle message source

--- src/main/java/com/example/slabiak/appointmentscheduler/config/WebMvcConfig.java
+++ src/main/java/com/example/slabiak/appointmentscheduler/config/WebMvcConfig.java
@@ -1,15 +1,11 @@
 package com.example.slabiak.appointmentscheduler.config;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -28,12 +24,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		final LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
 		localeInterceptor.setParamName("lang");
 		return localeInterceptor;
-	}
-
-	@Bean
-	public LocaleResolver localeResolver() {
-		final SessionLocaleResolver clr = new SessionLocaleResolver();
-		clr.setDefaultLocale(new Locale("ru"));
-		return clr;
 	}
 }
